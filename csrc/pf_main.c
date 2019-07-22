ea87b513e229bd06e3e26607f2c4b490fb4b7d4c
glenda <glenda@opti>
2019-07-21 23:02:30 -0500

    add floating point support

--- csrc/pf_main.c
+++ csrc/pf_main.c
@@ -44,6 +44,36 @@
 #define FALSE (0)
 #endif
 
+#ifdef PF_SUPPORT_FP
+/* FCR */
+#define	FPINEX	(1<<5)
+#define	FPUNFL	((1<<4)|(1<<1))
+#define	FPOVFL	(1<<3)
+#define	FPZDIV	(1<<2)
+#define	FPINVAL	(1<<0)
+#define	FPRNR	(0<<10)
+#define	FPRZ	(3<<10)
+#define	FPRPINF	(2<<10)
+#define	FPRNINF	(1<<10)
+#define	FPRMASK	(3<<10)
+#define	FPPEXT	(3<<8)
+#define	FPPSGL	(0<<8)
+#define	FPPDBL	(2<<8)
+#define	FPPMASK	(3<<8)
+/* FSR */
+#define	FPAINEX	FPINEX
+#define	FPAOVFL	FPOVFL
+#define	FPAUNFL	FPUNFL
+#define	FPAZDIV	FPZDIV
+#define	FPAINVAL	FPINVAL
+
+double
+round(double x)
+{
+	return floor(x+0.5);
+}
+#endif
+
 #ifdef PF_EMBEDDED
 int main( void )
 {
@@ -135,6 +165,10 @@ int main( int argc, char **argv )
         ERR(("pForth stopping on unit test failure.\n"));
         goto on_error;
     }
+#endif
+
+#ifdef PF_SUPPORT_FP
+	setfcr((FPINEX & ~FPINEX) | (FPOVFL & ~FPOVFL) | (FPUNFL & ~FPUNFL) | (FPINVAL & ~FPINVAL));
 #endif
 
     Result = pfDoForth( DicName, SourceName, IfInit);
